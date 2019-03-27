fea76391a2be79a40282e44fe484cc3cb385915e
Arthur Lima de Souza <souza.arthur@cwi.com.br>
2019-03-27 09:49:33 -0300

    feat(native-base-lib)

--- TavernaApp/src/layout/screens/home/home.screen.js
+++ TavernaApp/src/layout/screens/home/home.screen.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
-import { Platform, Text, View, Image } from 'react-native'
-
+import { Platform, View, Image } from 'react-native'
+import { Button, Text } from 'native-base';
 import styles from './home.style'
+import HeaderApp from '../../components/header.app';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -17,11 +18,15 @@ export class HomeScreen extends Component {
   }
 
   render() {
-    return (
+    return ( 
       <View style={styles.container}>
+        <HeaderApp/>
         <Text style={styles.welcome}>Welcome to Taverna RPG!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <Button rounded success>
+            <Text>Nothing happen</Text>
+          </Button>
       </View>
     );
   }
